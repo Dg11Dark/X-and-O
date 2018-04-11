7b0b40adc276ca442257cc3c490c63afb743cebf
Dg11Dark <38052549+Dg11Dark@users.noreply.github.com>
2018-04-11 21:29:58 +0300

    up5

--- XO/app/src/main/java/com/example/user/xo/SharedGames.java
+++ XO/app/src/main/java/com/example/user/xo/SharedGames.java
@@ -1,15 +1,19 @@
 package com.example.user.xo;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class SharedGames extends AppCompatActivity {
+public class SharedGames extends AppCompatActivity implements View.OnClickListener {
 
     TextView phone , games;
+    Button BtnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +21,21 @@ public class SharedGames extends AppCompatActivity {
         SharedPreferences myPref = getSharedPreferences("myPref", MODE_PRIVATE);
         phone = (TextView) findViewById(R.id.phone);
         games = (TextView) findViewById(R.id.games);
-        String names=myPref.getString("phone","nothing");
-        String numbers=myPref.getString("smsInfo","nothing");
+        BtnBack = (Button) findViewById(R.id.BtnBack);
+
+        String names = myPref.getString("phone", "nothing");
+        String numbers = myPref.getString("smsInfo", "nothing");
 
         phone.setText(names);
         games.setText(numbers);
+
+    }
+    @Override
+    public void onClick (View view){
+        if (view == BtnBack) {
+            Intent Stati = new Intent(this, Statistic.class);
+            startActivity(Stati);
+            }
+        }
     }
 
-
-
-}
